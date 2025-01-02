aa4c6f7d0739ffd37decf7d5c8d78955d5ed0273
Yfla <yoosw2001@snu.ac.kr>
2025-01-02 16:39:48 +0900

    1.3 patch
    Fix room_prgm rate calculator
    
    Fix initial condition confirm as receving name of program and room too. We can add more program and more room with this

--- elec_jebby.py
+++ elec_jebby.py
@@ -6,7 +6,7 @@ class elec_jebby:
     def __init__(self, seed=None, 
                  jebby_prgm=['master', 'doctor'], 
                  jebby_rate=[21, 37], 
-                 jebby_room=['204', '212B', '214', '304'], 
+                 jebby_room=['204', '212B', '214', '314'], 
                  jebby_seat=[14, 11, 19, 13]
                  ):
         np.random.seed(seed)
@@ -42,98 +42,189 @@ class elec_jebby:
         
         if react == 0:
             while True:
-                seed = input("Enter your seed : ")
-                seed = self.confirm_int(seed, errortext="Please enter an integer as a seed!", errorshow=True)
-                if seed is not None:
+                curr_seed = input("Enter your seed : ")
+                curr_seed = self.confirm_int(curr_seed, errortext="Please enter an integer as a seed!", errorshow=True)
+                if curr_seed is not None:
                     break
-            if type(seed) is int and seed >= 0:
-                np.random.seed(seed)
-                self.seed = seed
+            if type(curr_seed) is int and curr_seed >= 0:
+                np.random.seed(curr_seed)
+                self.seed = curr_seed
+                dict_init['seed'] = (prev_seed, curr_seed)
             else:
                 print("Please enter an integer larger than 0 as a seed!")
+        else:
+            dict_init['seed'] = (prev_seed, prev_seed)
         
-        dict_init['seed'] = prev_seed
+        prev_prgm, curr_prgm = self.jebby_prgm, []
+        react = input(f"Initial program set is {self.jebby_prgm}, do you want to switch this? (Yes: 0, No: all the others) : ")        
+        react = self.confirm_int(react)
+        
+        if react == 0:
+            while True:
+                time.sleep(self.td)
+                print("-------------------------------------------------------")
+                print(f"Current program set: {curr_prgm}")
+                print("If you want to interupt, enter the \'stop\'")
+                print("If you want to wrap up the program set recording, enter the \'end\'")
+                print("Z: Control Z")
+                print("-------------------------------------------------------")
+                prgm_name = input("Enter a program name : ")
+                if prgm_name == 'stop':
+                    dict_init['prgm'] = (prev_prgm, prev_prgm)
+                    break
+                elif prgm_name == 'end':
+                    dict_init['prgm'] = (prev_prgm, curr_prgm)
+                    self.jebby_prgm = curr_prgm
+                    break
+                elif prgm_name == 'Z' and len(curr_prgm) > 0:
+                    curr_prgm = curr_prgm[:-1]
+                elif prgm_name == 'Z' and len(curr_prgm) == 0:
+                    print("Current program set doesn\'t have any element!")
+                else:
+                    curr_prgm.append(prgm_name)
+        else:
+            curr_prgm = self.jebby_prgm
+            dict_init['prgm'] = (prev_prgm, curr_prgm)
         
         prev_rate, curr_rate = [], []
         for i, prgm in enumerate(self.jebby_prgm):
-            prev_rate.append(self.jebby_rate[i])
-            
-            while True:
+            try:
+                prev_rate.append(self.jebby_rate[i])
+            except IndexError:
+                pass
+
+            try:
                 react = input(f"Initial number of {prgm} is {self.jebby_rate[i]}, do you want to switch this? (Yes: 0, No: all the others) : ")
                 react = self.confirm_int(react)
-                if react is not None:
-                    break
+            except IndexError:
+                react = input(f"Initial number of {prgm} has no value, do you want to switch this? (Yes: 0, No: all the others -> Input 0) : ")
+                react = self.confirm_int(react)
             
             if react == 0:
                 while True:
-                    rate = input(f"Enter the number of {prgm} : ")
+                    rate = input(f"Enter the number of {prgm} : ")               
                     rate = self.confirm_int(rate, errortext=f"Please enter an interger as a number of {prgm}!", errorshow=True)
-                    if rate is not None:
+                    if rate is None:
+                        continue
+                    if rate < 0 and type(rate) is int:
+                        print("Please enter an integer larger than 0 as a number!")
+                        continue
+                    elif type(rate) is int:
+                        curr_rate.append(rate)
                         break
-                if rate < 0 and type(rate) is int:
-                    print("Please enter an integer larger than 0 as a number!")
-                    curr_rate.append(self.jebby_rate[i])
-                elif type(rate) is int:
-                    curr_rate.append(rate)
             else:
-                curr_rate.append(self.jebby_rate[i])
+                try:
+                    curr_rate.append(self.jebby_rate[i])
+                except IndexError:
+                    curr_rate.append(0)
         
-        dict_init['rate'] = prev_rate
+        dict_init['rate'] = (prev_rate, curr_rate)
         self.jebby_rate = curr_rate
+        
+        prev_room, curr_room = self.jebby_room, []
+        react = input(f"Initial room set is {self.jebby_room}, do you want to switch this? (Yes: 0, No: all the others) : ")        
+        react = self.confirm_int(react)
+        
+        if react == 0:
+            while True:
+                time.sleep(self.td)
+                print("-------------------------------------------------------")
+                print(f"Current room set: {curr_room}")
+                print("If you want to interupt, enter the \'stop\'")
+                print("If you want to wrap up the program set recording, enter the \'end\'")
+                print("Z: Control Z")
+                print("-------------------------------------------------------")
+                room_name = input("Enter a room name : ")
+                if room_name == 'stop':
+                    dict_init['room'] = (prev_room, prev_room)
+                    break
+                elif room_name == 'end':
+                    dict_init['room'] = (prev_room, curr_room)
+                    self.jebby_room = curr_room
+                    break
+                elif room_name == 'Z' and len(curr_room) > 0:
+                    curr_room = curr_room[:-1]
+                elif room_name == 'Z' and len(curr_room) == 0:
+                    print("Current program set doesn\'t have any element!")
+                else:
+                    curr_room.append(room_name)
         
         prev_seat, curr_seat = [], []
         for i, room in enumerate(self.jebby_room):
-            prev_seat.append(self.jebby_seat[i])
+            try:
+                prev_seat.append(self.jebby_seat[i])
+            except IndexError:
+                pass
             
-            while True:
+            try:
                 react = input(f"Initial seat of {room} is {self.jebby_seat[i]}, do you want to switch this? (Yes: 0, No: all the others) : ")
                 react = self.confirm_int(react)
-                if react is not None:
-                    break
+            except IndexError:
+                react = input(f"Initial number of {room} has no value, do you want to switch this? (Yes: 0, No: all the others -> Input 0) : ")
+                react = self.confirm_int(react)
             
             if react == 0:
                 while True:
-                    seat = input(f"Enter the number of {room} : ")
-                    seat = self.confirm_int(seat, errortext=f"Please enter an interger as a number of seats of {room}!", errorshow=True)
-                    if seat is not None:
+                    seat = input(f"Enter the number of {room} : ")                    
+                    seat = self.confirm_int(seat, errortext=f"Please enter an interger as a number of {prgm}!", errorshow=True)
+                    if seat is None:
+                        continue
+                    if seat < 0 and type(seat) is int:
+                        print("Please enter an integer larger than 0 as a number!")
+                        continue
+                    elif type(seat) is int:
+                        curr_seat.append(seat)
                         break
-                if seat < 0 and type(seat) is int:
-                    print("Please enter an integer larger than 0 as a number!")
-                    curr_seat.append(self.jebby_seat[i])
-                elif type(seat) is int:
-                    curr_seat.append(seat)
             else:
-                curr_seat.append(self.jebby_seat[i])
+                try:
+                    curr_seat.append(self.jebby_seat[i])
+                except IndexError:
+                    curr_seat.append(0)
         
-        dict_init['seat'] = prev_seat
+        dict_init['seat'] = (prev_seat, curr_seat)
         self.jebby_seat = curr_seat
+        
+        if sum(curr_rate) > sum(curr_seat):
+            print('Caution! : The total number of people exceed number of seats!')
         
         self.act_history.append(dict_init)
         self.act_name.append("init_confirm")
         
     def room_prgmnum_calculator(self):
-        seatlist = []
+        prgmlist, alphalist = [], []
         for i, room in enumerate(self.jebby_room):
             seatnum = self.jebby_seat[i]
             
             prgmfloat = seatnum * np.array(self.jebby_rate) / np.sum(self.jebby_rate)
             prgmint = prgmfloat.astype(int)
             alphanum = prgmfloat - prgmint
-            alphasum, alphasort = int(np.sum(alphanum)), np.argsort(alphanum)
+            
+            prgmlist.append(prgmint)
+            alphalist.append(alphanum)
+            '''alphasum, alphasort = int(np.sum(alphanum)), np.argsort(alphanum)
             remain = np.zeros(len(self.jebby_prgm))
             remain[np.argsort(alphasort)[::-1][:alphasum]] = 1
             
             prgmnum = prgmint + remain
-            seatlist.append(prgmnum)
-            
-            for j, prgm in enumerate(self.jebby_prgm):
-                print(f'program {prgm} in room {room} : {prgmnum[j]} seats!')
+            seatlist.append(prgmnum)'''
         
-        self.seatnum_arr = np.array(seatlist).T
-        historyshow = []
+        prgmarr, alphaarr = np.array(prgmlist), np.array(alphalist)
+        
+        for j, prgm in enumerate(self.jebby_prgm):
+            prgm_lack = int(self.jebby_rate[j] - np.sum(prgmarr.T[j]))
+            remain = np.zeros(len(self.jebby_room), dtype=int)
+            remain[np.argsort(alphaarr.T[j])[::-1][:prgm_lack]] = 1
+            prgmarr.T[j] += remain
+            
         for i, room in enumerate(self.jebby_room):
-            historyshow.append(room)
-            historyshow.append(tuple(seatlist[i].astype(int)))
+            for j, prgm in enumerate(self.jebby_prgm):
+                print(f'program {prgm} in room {room} : {prgmarr[i][j]} seats!')
+        
+        self.seatnum_arr = prgmarr.T
+        historyshow = []
+        for j, prgm in enumerate(self.jebby_prgm):
+            historyshow.append(prgm)
+            historyshow.append(tuple(prgmarr.T[j].astype(int)))
         self.act_history.append(tuple(historyshow))
         self.act_name.append("room_prgm_num")
         
@@ -268,9 +359,11 @@ class elec_jebby:
     def ctrlz(self):
         actname, act = self.act_name[-1], self.act_history[-1]
         if actname == 'init_confirm':
-            self.seed = act['seed']
-            self.jebby_rate = act['rate']
-            self.jebby_seat = act['seat']
+            self.seed = act['seed'][0]
+            self.jebby_prgm = act['prgm'][0]
+            self.jebby_rate = act['rate'][0]
+            self.jebby_room = act['room'][0]
+            self.jebby_seat = act['seat'][0]
             time.sleep(self.td)
         elif actname == 'room_prgm_num':
             self.seatnum_arr = None
